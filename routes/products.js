ee9d91649362f40abc8b937ffded858034a6a851
Waqar Ahmed <54082156+waqar-ahmed51@users.noreply.github.com>
2022-08-02 15:06:08 +0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -6,65 +6,65 @@ const router = express.Router();
 const Product = require('../models/Product');
 
 // Getting all the Products
-router.get('/', async (req, res)=>{
+router.get('/', async (req, res) => {
     // res.send("Product Page - Shows JSON");
     try {
-        const products= await Product.find();
+        const products = await Product.find();
         res.json(products);
     } catch (error) {
-        res.json({messge: error})
+        res.json({ messge: error })
     }
 });
 
 //Submitting a Product
-router.post('/', async (req,res)=>{
+router.post('/', async (req, res) => {
     const product = new Product({
         id: req.body.id,
         title: req.body.title,
         desc: req.body.desc,
         img: req.body.img,
         price: req.body.price,
-        priceQuantity:req.body.priceQuantity,
-        quantity:req.body.quantity,
-        category:req.body.category,
-        popular:req.body.popular,
+        priceQuantity: req.body.priceQuantity,
+        quantity: req.body.quantity,
+        category: req.body.category,
+        popular: req.body.popular,
         "Main Features": req.body["Main Features"],
     });
     try {
         const submitProduct = await product.save();
-        res.json(submitProduct);    
+        res.json(submitProduct);
     } catch (error) {
-        res.json({messge: error})
+        res.json({ messge: error })
     }
 });
 
 //Get Single Product
-router.get('/:productId', async (req,res)=>{
+router.get('/:productId', async (req, res) => {
     try {
         const product = await Product.findById(req.params.productId);
         res.json(product);
     } catch (error) {
-        res.json({messge: error})
+        res.json({ messge: error })
     }
 });
 
 // Delete a Product
-router.delete('/:productId', async (req,res)=>{
+router.delete('/:productId', async (req, res) => {
     try {
-        const deleteProduct = await Product.remove({_id:req.params.productId});
+        const deleteProduct = await Product.remove({ _id: req.params.productId });
         res.json(deleteProduct);
     } catch (error) {
-        res.json({messge: error})
+        res.json({ messge: error })
     }
 });
 
 // Update a Product
-router.patch('/:productId', async (req,res)=>{
+router.patch('/:productId', async (req, res) => {
     try {
-        const updateProduct = await Product.updateOne({_id:req.params.productId}, {$set : {title: req.body.title}});
+        const updateProduct = await Product.updateOne({ _id: req.params.productId }, { $set: { title: req.body.title } });
         res.json(updateProduct);
     } catch (error) {
-        res.json({messge: error})
+        res.json({ messge: error })
     }
 });
-   module.exports = router;
+module.exports = router;
